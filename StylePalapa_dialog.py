117f36e20313dd11a77c17f7915ce155445eed27
sandynagara <tausan22@gmail.com>
2021-12-31 11:35:57 +0700

    tes

--- StylePalapa_dialog.py
+++ StylePalapa_dialog.py
@@ -3,6 +3,7 @@ import os
 from qgis.PyQt import uic
 from qgis.PyQt import QtWidgets
 from qgis.core import QgsProject
+from qgis.PyQt.QtWidgets import QFileDialog
 
 # This loads your .ui file so that PyQt can populate your plugin with the elements from Qt Designer
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
@@ -15,9 +16,16 @@ class StylePalapaDialog(QtWidgets.QDialog, FORM_CLASS):
         super(StylePalapaDialog, self).__init__(parent)
         self.setupUi(self)
         self.upload.clicked.connect(self.import_layer)
+        self.browser.clicked.connect(self.import_meteadata)
       
     def import_layer(self):
         layerName = self.select_layer.currentText()
+        print(layerName)
         layer = QgsProject().instance().mapLayersByName(layerName)[0]
         layer.saveSldStyle(f'D:/{layerName}.sld')
         os.remove(f'D:/{layerName}.sld')
+
+    def import_meteadata(self):
+        filename1, text = QFileDialog.getOpenFileName()
+        self.select_layer.setText(filename1)
+        print(filename1)
